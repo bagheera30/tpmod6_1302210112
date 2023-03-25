d8992a08b45b5339d1fbe7aaa2e96a327423ef58
bagheera30 <rizkialfian30103@gmail.com>
2023-03-25 18:15:05 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210112/Program.cs
+++ tpmodul6_1302210112/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302210112;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - muhammad rizki alfian");
+        video.IncreasePlayCount(20);
+        video.printVideoDetail();
+    }
+}
---
+++ tpmodul6_1302210112/SayaTubeVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210112
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+        public SayaTubeVideo(string title)
+        {
+            this.id=new Random().Next(10000, 99999);
+            this.title=title;
+            this.playcount=0;
+        }
+        public void IncreasePlayCount(int input)
+        {
+            this.playcount += input;
+        }
+        public void printVideoDetail()
+        {
+            Console.WriteLine("Video ID: {0}", this.id);
+            Console.WriteLine("Title: {0}", this.title);
+            Console.WriteLine("Play Count: {0}", this.playcount);
+        }
+    }
+}
